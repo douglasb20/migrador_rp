22bd4c118b9b40c8347ccff5687f859c1f3a5756
Douglas A. Silva <douglaassgenesis@gmail.com>
2023-03-18 20:27:29 -0300

    primeiras configuracoes

--- MigradorRP/Form1.Designer.cs
+++ MigradorRP/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MigradorRP
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTopBar = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTopBar
+            // 
+            this.lblTopBar.Font = new System.Drawing.Font("Poppins Medium", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopBar.ForeColor = System.Drawing.Color.White;
+            this.lblTopBar.Location = new System.Drawing.Point(0, 0);
+            this.lblTopBar.Name = "lblTopBar";
+            this.lblTopBar.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.lblTopBar.Size = new System.Drawing.Size(730, 40);
+            this.lblTopBar.TabIndex = 0;
+            this.lblTopBar.Text = "Automatec Sistemas | MigradorRP";
+            this.lblTopBar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTopBar_MouseMove);
+            // 
+            // lblClose
+            // 
+            this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.ForeColor = System.Drawing.Color.White;
+            this.lblClose.Location = new System.Drawing.Point(794, 0);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(40, 40);
+            this.lblClose.TabIndex = 1;
+            this.lblClose.Text = "X";
+            this.lblClose.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            this.lblClose.MouseEnter += new System.EventHandler(this.lblClose_MouseEnter);
+            this.lblClose.MouseLeave += new System.EventHandler(this.lblClose_MouseLeave);
+            // 
+            // lblMin
+            // 
+            this.lblMin.BackColor = System.Drawing.Color.Transparent;
+            this.lblMin.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.ForeColor = System.Drawing.Color.White;
+            this.lblMin.Location = new System.Drawing.Point(749, 0);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.lblMin.Size = new System.Drawing.Size(40, 40);
+            this.lblMin.TabIndex = 2;
+            this.lblMin.Text = "-";
+            this.lblMin.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblMin.Click += new System.EventHandler(this.lblMin_Click);
+            this.lblMin.MouseEnter += new System.EventHandler(this.lblMin_MouseEnter);
+            this.lblMin.MouseLeave += new System.EventHandler(this.lblMin_MouseLeave);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(834, 481);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.lblTopBar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Automatec Sistemas | MigradorRP";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTopBar;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Label lblMin;
     }
 }
 
--- MigradorRP/Form1.cs
+++ MigradorRP/Form1.cs
@@ -3,18 +3,109 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MigradorRP
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+            this.Paint += new PaintEventHandler(frmMain1_Paint);
+        }
+
+        private void frmMain1_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath forma = new GraphicsPath();
+            forma.StartFigure();
+            forma.AddArc(new Rectangle(0, 0, 20, 20), 180, 90);
+            forma.AddLine(20, 0, this.Width - 20, 0);
+            forma.AddArc(new Rectangle(this.Width - 20, 0, 20, 20), -90, 90);
+            forma.AddLine(this.Width, 20, this.Width, this.Height - 20);
+            forma.AddArc(new Rectangle(this.Width - 20, this.Height - 20, 20, 20), 0, 90);
+            forma.AddLine(this.Width - 20, this.Height, 20, this.Height);
+            forma.AddArc(new Rectangle(0, this.Height - 20, 20, 20), 90, 90);
+            forma.CloseFigure();
+            this.Region = new Region(forma);
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            const int WM_NCPAINT = 0x85;
+            if (m.Msg == WM_NCPAINT)
+            {
+                IntPtr hdc = GetWindowDC(m.HWnd);
+                if ((int)hdc != 0)
+                {
+                    Graphics g = Graphics.FromHdc(hdc);
+                    g.FillRectangle(Brushes.Green, new Rectangle(0, 0, 4800, 23));
+                    g.Flush();
+                    ReleaseDC(m.HWnd, hdc);
+                }
+            }
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void moverForm()
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void lblMin_MouseEnter(object sender, EventArgs e)
+        {
+            lblMin.BackColor = Color.FromArgb(54,54,54);
+        }
+
+        private void lblMin_MouseLeave(object sender, EventArgs e)
+        {
+            lblMin.BackColor = Color.Transparent;
+        }
+
+        private void lblClose_MouseEnter(object sender, EventArgs e)
+        {
+            lblClose.BackColor = Color.FromArgb(188, 75, 81);
+        }
+
+        private void lblClose_MouseLeave(object sender, EventArgs e)
+        {
+            lblClose.BackColor = Color.Transparent;
+        }
+
+        private void lblTopBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            moverForm();
         }
     }
 }
